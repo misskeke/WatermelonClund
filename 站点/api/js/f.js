a39b9a685637d067f6e2f9f1705b4cecc8ef29ba
王庭茂 <wtmtim@126.com>
2014-06-30 19:23:49 +0800

    去除所有关于加密的代码。。
    已经可以搞https了。
    删除了 一堆 代码。
    
    Signed-off-by: 王庭茂 <wtmtim@126.com>

--- 站点/api/js/f.js
+++ 站点/api/js/f.js
@@ -1,36 +1,14 @@
 (function() {
 	function xapilog(str) {
-		console.debug("[" + new Date().toTimeString().substr(0, 8) + "|XAPI] - " + str);
+		console.info("[" + new Date().toTimeString().substr(0, 8) + "|XAPI] - " + str);
+		$('.xapi_log_shower').text(str);
 	}
+
 	xapilog("loaded!");
 	window.XAPI = {
-		log: xapilog
+		log : xapilog
 	};
-	XAPI.log("loading RSA api...");
-	$.getScript("UI/JS/lib/rsa.js", function() {
-		XAPI.log("RSA Api loaded");
-		$.get("api/rsa_get_public_key/", function(rsap) {
-			XAPI.log("rsa public key getted! e=" + rsap.e.substr(0, 6) + "..., m=" + rsap.m.substr(0, 6) + "...");
-			setMaxDigits(259);
-			XAPI.rsakey = new RSAKeyPair(rsap.e, "", rsap.m);
-			XAPI.log("loading AES...");
-			$.getScript("UI/JS/lib/aes.js", function() {
-				XAPI.log("AES Api loaded");
-				XAPI.log("loading user api...");
-                XAPI.randStr = function(len) {
-						len = len || 32;
-						var $chars = '0123456789abcdefghijklmnopqrstovwxyz+=-[]()\/';
-						var maxPos = $chars.length;
-						var pwd = '';
-						for (i = 0; i < len; i++) {
-							pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
-						}
-						return pwd;
-					}
-				$.getScript("api/js/user.js", function() {
-					XAPI.log("User Api loaded");
-				});
-			});
-		}, "json");
+	$.getScript("api/js/user.js", function() {
+		XAPI.log("User Api loaded");
 	});
-})();
+})(); 
