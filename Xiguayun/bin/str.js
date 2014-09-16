825ab919a047a47686e2c2a978273a3fff3f6ea6
王庭茂 <wtmtim@126.com>
2014-09-16 11:15:19 +0800

    Use crypto for md5
    removed md5

--- Xiguayun/bin/str.js
+++ Xiguayun/bin/str.js
@@ -1,22 +1,23 @@
-var m_md5,m_crypto,shaer;
+var m_crypto,shaer,md5er;
 module.exports={
     cache:null,
     init:function(c){
         this.cache=c;
-        m_md5=require('MD5');
         m_crypto = require('crypto');
-        shaer=m_crypto.createHash('sha512');
     },
     strsftrim:function(s){
         return s.trim().replace(/[\x00-\x19\x7F-\xA0\u1680\u180E\u2000-\u200B\u2028\u2029\u202F\u205F\u3000\uFEFF]/g,'').trim();
     },
     md5:function(s){
         return this.cache.get("md5\x00"+ s,function(){
-            return m_md5(s);
+            md5er=m_crypto.createHash('md5');
+            md5er.update(s);
+            return md5er.digest('hex');
         });
     },
     sha512:function(s){
         return this.cache.get("sha512\x00"+ s,function(){
+            shaer=m_crypto.createHash('sha512');
             shaer.update(s);
             return shaer.digest('hex');
         });
