3c5eb3e9a589e3853a17824c51b58504b95592a3
maomao <wtmtim@126.com>
2014-11-02 14:32:07 +0800

    a

--- Xiguayun/bin/xAddon.js
+++ Xiguayun/bin/xAddon.js
@@ -4,7 +4,7 @@ module.exports = function(mon,dbc,strlib,ersp){
             var ddc = req.params.dl;
             switch (ddc.substr(1)){
                 case "markedit":
-                    res.render('xgycoxmark', {title: "Xmark - 文本托管服务"});
+                    res.render('xgycoxmark', {title: "Markbox"});
                     break;
                 default :
                     var ep = new Error("项目页面：不存在");
