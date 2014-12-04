0ed8b0a08638b1d0c53a0e12ed52ae5305ea8ed3
Wtm <wtmtim@126.com>
2014-12-04 22:01:56 +0800

    - -

--- xgyco-ext-chrome/b.js
+++
@@ -1,8 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-		var dtd = details.url.match(/^(.+:\/\/){0,1}\.([^/\\]+)/);
-        if( dtd )
-            return {redirectUrl: "xgy.co/"+dtd[2] };
-    },
-    {urls: ["*://*/"]},
-    ["blocking"]);
